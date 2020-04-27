4b14fcd0011e91a76de5d70ff61f59f7d29b5bc0
George Ruck <georgeadruck@gmail.com>
2020-04-27 18:36:19 +0200

    Final comments of readfile for easier understanding of code

--- Code/begles_readfile.py
+++ Code/begles_readfile.py
@@ -74,62 +74,58 @@ def merge_arrays(df_left,df_right,method = 'left', join_column = 'datetime'):
 #%% Main code
     
 if __name__ == "__main__":
-    """
-    create a main code with the path as an input!!
-    """
+    #general plotting style
     sns.set()
-    sns.set_context('talk')
-    #%% locate and read data file
-    path = r'C:\Users\gruck\Documents\Begles' #à modifier 
+    sns.set_context('talk')     # a modifier selon gout
+    
+    # locate and read data file - could use np array if efficiency is of concern. Here pandas fine.
+    path = os.getcwd()          #à modifier 
     file = 'VILLE DE BEGLES_HOTEL DE VILLE_RAE_30001611162862_CDC_22_09_2018_au_21_12_2018.xlsx'
     df = read_file(path,file,sheet_name = 'HOTEL DE VILLE_Pts_10')
 
     #rename the column currently labelled JJ/MM/AAAA HH:MM:SS as 'datetime' (the first column in the list of columns)
     df.rename(columns = {list(df)[0] : 'datetime'},inplace = True)
     
-    #read second file with temperature data
+    #read second file with temperature data for full year this time
     df_temp = pd.read_csv('donnees_temp_bordeaux_2018.csv',delimiter = ';')
-    #%%change the date column from strings to recognised datetime object
+    #change date column from string to datetime object
     df_temp['Date'] =  pd.to_datetime(df_temp['Date'], format='%d/%m/%Y %H:%M') #format can be customised
-    df_temp.sort_values(by=['Date'],inplace = True)                             #dates were not in the correct order
-    df_temp.rename(columns = {'Date' : 'datetime'},inplace = True)              #same column name for both dataframes
+    df_temp.sort_values(by=['Date'],inplace = True)                             #dates not often in correct order
+    df_temp.rename(columns = {'Date' : 'datetime'},inplace = True)              #same column name in both dfs
     
     
-    #%% store original array as original in case reference is necessary
-    
+    # store original array before addition of prediction data
     df_original = df.set_index('datetime') #index of original dataframe is the date
-    
-    #%%
     
     #join the two arrays
     df = merge_arrays(df,df_temp)
-    df['Temperature'].interpolate(inplace = True)
-    df.dropna(inplace = True)                       #only occurs at brief intervals at beginning and end of dataset
+    df['Temperature'].interpolate(inplace = True)   #match timestep in factors
+    df.dropna(inplace = True)                       #drop small amount of nan values
     
-    
-    #%% plot four subplots to have an idea of the data
-    
+
     #add column for weekday 0(monday) - 6(sunday)
     df['weekday'] = df['datetime'].dt.dayofweek #reliant on datetime format
     
-    #jours férié - jeudi 1 NOV (only one jour férié in training set)
+    #change day of week to 6 for any bank holidays (only 1 in this time period)
     jour_fer('11-01',df)
     
-    df['heure'] = df['datetime'].dt.hour                                    #column for hour
-    df['heure minutes'] = df['datetime'].dt.strftime('%H:%M')               #column for hour minutes
-    df['month'] = df['datetime'].dt.month
+    #
+    df['heure'] = df['datetime'].dt.hour                            #column for hour
+    df['heure minutes'] = df['datetime'].dt.strftime('%H:%M')       #column for hour minutes
+    df['month'] = df['datetime'].dt.month                           #column for month
     
     df['echauffement'] = df['datetime']>datetime.datetime(2018,11,20,0,0)   #périod d'échauffe (commencant le 20 novembre)
     df.set_index('datetime', inplace = True)                                #set index as date after extra columns formed
     
-    #heure minutes (hm) as a continuous scale
+    #heure minutes (hm) as a continuous scale (better for ML algorithm)
     df['hm'] = df['heure minutes'].apply(lambda x: int(100*int(x.split(':')[0])+(5/3)*int(x.split(':')[1])))
     
-    #save file
-    df.to_csv(r'begles_3month_Data.csv',sep = ';')
+    #save file for use in ML algorithm UNCOMMENT BELOW IF NEVER RUN BEFORE
+    # df.to_csv(r'begles_3month_Data.csv',sep = ';')
     
     #%% plots
     
+    #start with 4*4 grid for initial view
     fig_sig,ax_sig = plt.subplots(2,2,figsize = (18,10)) 
     plt.suptitle('Données 01/09 - 20/12 2018')
     
@@ -155,15 +151,15 @@ if __name__ == "__main__":
     #tight layout
     fig_sig.tight_layout(rect = [0, 0.03, 1, 0.95])
     
+    #UNCOMMENT IF NEVER RUN
     #fig_sig.savefig('firstPlots.png')
     
-    #%% output df - creat a df with the columns minus first two for the unknown power months that we want to predict
+    #%% output df - create df with the columns without Y_pred column
     
+    #add appropriate index to new df with times (up to end of August here)
     numdays,numdays_end = 243,9
     date_list = [df.index[0] - datetime.timedelta(minutes = x) - datetime.timedelta(hours = 2) for x in np.linspace(0,numdays*24*6*10,numdays*24*6+1)][::-1]
     date_list_end = [df.index[-1] + datetime.timedelta(minutes = x) for x in np.linspace(10,numdays_end*24*6*10+10,numdays_end*24*6+1)]
-    
-    #create dataframe with dates for up to end of august as values
     
     df_pred = pd.DataFrame(np.hstack((date_list,date_list_end)),
                            columns = ['datetime'])
@@ -174,46 +170,53 @@ if __name__ == "__main__":
     #add column for weekday
     df_pred['weekday'] = df_pred['datetime'].dt.dayofweek
     
-    #jours férié - 
+    #jours fériés - more to add for this part of the year
     joursferies = '01-01 04-02 05-01 05-08 05-10 05-22 07-14 08-15 12-25'.split()
     for jour in joursferies:
         jour_fer(jour,df_pred)
     
+    #remove any nan values
     df_pred.dropna(inplace = True)
     
+    #format df appropriately
     df_pred['heure'] = df_pred['datetime'].dt.hour
     df_pred['heure minutes'] = df_pred['datetime'].dt.strftime('%H:%M')
     df_pred['month'] = df_pred['datetime'].dt.month
     
+    #here I also add supp. heating period until some point in April
     df_pred['echauffement'] = (df_pred['datetime']<datetime.datetime(2018,4,20,0,0))|(df_pred['datetime']>datetime.datetime(2018,11,20,0,0))
     
-    #minutes en decimal de l'heure (fraction of an hour)
+    #hour minute column to continuous non discrete scale
     df_pred['hm'] = df_pred['heure minutes'].apply(lambda x: int(100*int(x.split(':')[0])+(5/3)*int(x.split(':')[1])))
     
+    #sort according to date time
     df_pred.sort_values(by=['datetime'],inplace = True)
     df_pred.set_index('datetime', inplace = True)
     
     
-    #%% arbre decisionel
+    #%% arbre decisionel - decision tree regressor
     
     from sklearn.tree import DecisionTreeRegressor
     #from sklearn.tree import export_graphviz
     
     from sklearn.ensemble import AdaBoostRegressor
     
-    
+    #input predictor variables to X df and output to Y
     X = df[['Temperature','weekday','echauffement','hm']]
     Y = df['PS ATTEINTE']
     
+    #Boosting method (here has little effect as area under curve largely unchanged)
     tree = AdaBoostRegressor(DecisionTreeRegressor(max_depth=15), n_estimators=20)
     tree.fit(X,Y)
     
+    # CODE BELOW CAN BE USED TO VISUALISE OUTPUT TREE (text.txt somewhere in repo)
     #export_graphviz(tree, out_file = 'tree.txt')
     
-    #%%
+    # Split test and train model accordingly
     from sklearn.model_selection import train_test_split
     X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33)
     
+    #decide on depth of tree to avoid overfit (elbow method) - I used 15 here see 209
     scores = []
     for i in range(1,20):
         tree1 = DecisionTreeRegressor(max_depth=i)
@@ -222,14 +225,11 @@ if __name__ == "__main__":
         valid_pred = tree1.predict(X_test)
         scores.append(tree1.score(X_test,Y_test))
     
-    #%%
-    
+    # perform predictions and validation testing
     train_pred = tree.predict(X)
     test_pred = tree.predict(df_pred[['Temperature','weekday','echauffement','hm']])
-    #for underestimate multiply by empiric factor
     
-    #%% PLOTS
-    
+    #%% PLOTS - see axis / titles for descriptions
     fig,ax = plt.subplots(2,1)
     fig.suptitle('September to December')
     ax[0].plot(df['PS ATTEINTE'])
@@ -238,7 +238,7 @@ if __name__ == "__main__":
     plt.figure()
     plt.plot(test_pred)
     
-    #%% Final Plot
+    # Final Plot
     
     plt.figure()
     plt.plot(df_pred.index,test_pred,color = 'dodgerblue')
@@ -247,19 +247,20 @@ if __name__ == "__main__":
     plt.xlabel('Date')
     plt.ylabel(r'P / $kW$')
     
-    #%% Create full data set
+    # Create full data frame with both prediction and existing data for full year plot
     
     df_pred['PS ATTEINTE'] = test_pred
     data2018 = pd.DataFrame(pd.concat([df_pred,df]))
     data2018.sort_index(inplace = True)
     
-    #%% VALIDATION
+    #%% VALIDATION - I had the energy bills over that year to predict the energy bills
     factures = [38834,49710,45601,38196,20017,15536,16726,13784,17302,11769,25547,40227]
     df_ps = data2018.reset_index()
     df_ps.set_index('month',inplace = True)
     energy_use = []
-    for i in range(1,13): energy_use.append(np.trapz(df_ps.loc[i]['PS ATTEINTE'])//6)
+    for i in range(1,13): energy_use.append(np.trapz(df_ps.loc[i]['PS ATTEINTE'])//6)   #numerical int of curve
     
+    #for plotting add validation results to df
     df_validation = pd.DataFrame({'factures':factures,'previsions':energy_use})
     df_validation['month'] = 'J F M A M J J A S O N D'.split()
     
@@ -267,6 +268,7 @@ if __name__ == "__main__":
     plt.ylabel('kWh facturés')
     plt.title('Consommation calculée vs Consommation calculée')
     
+    #check correlation values of model (very high in this case)
     correlations = {'annee':df_validation.corr(),'donnees':df_validation.iloc[8:].corr()}
     
     print('SUM of FACTURES: {},\n Sum of Energy USE: {}'.format(sum(factures),sum(energy_use)))
